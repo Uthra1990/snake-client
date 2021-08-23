5079dff5032c36c2cbcdaf20a61a67376ed7a16c
Uthra1990 <uthraravichandran29@gmail.com>
2021-08-23 00:17:03 -0400

    exported setupInput

--- play.js
+++ play.js
@@ -1,22 +1,8 @@
 const {connect} = require('./client.js')
+const { setupInput } = require('./input.js')
 
+console.log('Connecting ...');
+connect();
 
+setupInput();
 
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  const handleUserInput = function() {
-    process.stdin.on('data', (key) => {
-    if (key === '\u0003') {
-      process.exit(); }
-    });
-  }
-  return stdin;
-}
-
-
-setupInput()
-
-// establishes a connection with the game server
